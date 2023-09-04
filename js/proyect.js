83baf675ee8adb6a223da1b56900767764b97a93
ignacio talon <talonignacio07@gmail.com>
2023-09-04 15:13:28 -0300

    primera entra js

---
+++ js/proyect.js
@@ -0,0 +1,5 @@
+const nombre = prompt("Inserta tu nombre");
+const anioNaci = prompt("Inserta tu año de nacimiento");
+const edad = anioNaci - 2023;
+
+alert("te llamas " + nombre + " y tenes " + edad + " años.");
